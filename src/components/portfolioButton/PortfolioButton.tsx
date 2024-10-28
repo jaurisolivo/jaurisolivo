6e72c90cff64cfdfa0b2994214606a1a7fde5e05
Jay <81289210+thejayblue@users.noreply.github.com>
2024-10-28 09:30:06 +0100

    feat(ui): add down-arrow in portfolio-btn

--- src/components/portfolioButton/PortfolioButton.tsx
+++ src/components/portfolioButton/PortfolioButton.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
+import Image from "next/image";
+
 import styles from "./PortfolioButton.module.scss";
 
-
-
-const PortfolioButton= () => {
+const PortfolioButton = () => {
 	return (
 		<div className={styles.btnContainer}>
-			<Link href={"#portfolio"} className={styles.btn}>Portfolio</Link>
+			<Link href={"#portfolio"} className={styles.btn}>
+				<span>Portfolio</span>
+                <Image src="/assets/icons/down-arrow.svg" alt="" width={18} height={18} quality={100} />
+			</Link>
 		</div>
 	);
 };
